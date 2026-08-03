ba8b426d91e5f1aa0d4630fbe9bbeaaae898d2e4
yk-chubu <ykitaura@gs4e.chubu.ac.jp>
2026-08-03 15:10:21 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,423 @@
+window.OC_DATA = {
+  "baseUrl": "https://yk-chubu.github.io/2026_summer_oc_rally/",
+  "dates": {
+    "7": "2026年8月7日（金）",
+    "8": "2026年8月8日（土）",
+    "9": "2026年8月9日（日）"
+  },
+  "labs": [
+    {
+      "id": "takei",
+      "name": "武井史郎研究室",
+      "building": "30号館",
+      "floor": "11F",
+      "days": [
+        7,
+        8,
+        9
+      ],
+      "fields": "魚類生物学／組織細胞化学／電子顕微鏡学",
+      "dept": "環境生物科学科",
+      "passwordHash": "a22cbc5cc261a03fe03749679eea961caeaad74cb79dbaedca46e265331f4538"
+    },
+    {
+      "id": "horibe",
+      "name": "堀部貴紀研究室",
+      "building": "30号館",
+      "floor": "11F",
+      "days": [
+        7,
+        8
+      ],
+      "fields": "園芸学／植物生理学／植物工場",
+      "dept": "環境生物科学科",
+      "passwordHash": "acd57b68057999515199f5d4e9ff46af9806093baf525e7255f45c7ecc75c9e0"
+    },
+    {
+      "id": "sumi",
+      "name": "墨 泰孝研究室",
+      "building": "30号館",
+      "floor": "11F",
+      "days": [
+        8
+      ],
+      "fields": "土壌学／植物栄養学／土壌微生物学",
+      "dept": "環境生物科学科",
+      "passwordHash": "a7d370f318e22325058e149d78876dde0c3a93251f42211efa1ab9cfecef9d24"
+    },
+    {
+      "id": "oba",
+      "name": "大場裕一研究室",
+      "building": "30号館",
+      "floor": "10F",
+      "days": [
+        7,
+        9
+      ],
+      "fields": "発光生物学／分子生態学",
+      "dept": "環境生物科学科",
+      "passwordHash": "f715e680550cbaeea2f59edd78fd6ebe8bc84b4376b9c74acebd3ebac74bda02"
+    },
+    {
+      "id": "hasegawa",
+      "name": "長谷川浩一研究室",
+      "building": "30号館",
+      "floor": "9F",
+      "days": [
+        7,
+        9
+      ],
+      "fields": "進化生物学／植物保護科学／遺伝学",
+      "dept": "環境生物科学科",
+      "passwordHash": "ca15d01c5f339fba76d9d23c20b27d99cfc22edc04d144f27a2e491739c3eeee"
+    },
+    {
+      "id": "suzuki",
+      "name": "鈴木孝征研究室",
+      "building": "30号館",
+      "floor": "9F",
+      "days": [
+        9
+      ],
+      "fields": "植物（シロイヌナズナ）／遺伝子（DNA）／バイオインフォマティクス",
+      "dept": "応用生物化学科",
+      "passwordHash": "6a541f28a8f99c2ed69e985b3ce96e1339c558d08d35ee5b64ee08dbebb29a8f"
+    },
+    {
+      "id": "matsubara",
+      "name": "松原和純研究室",
+      "building": "30号館",
+      "floor": "8F",
+      "days": [
+        7,
+        8,
+        9
+      ],
+      "fields": "細胞遺伝学／生態遺伝学／分子系統学",
+      "dept": "環境生物科学科",
+      "passwordHash": "1133158191cd2ab0a3f648ec103e7bf54a7f716bb8ad8a4a40962f45ef601b5a"
+    },
+    {
+      "id": "aichi",
+      "name": "愛知真木子研究室",
+      "building": "30号館",
+      "floor": "8F",
+      "days": [
+        7
+      ],
+      "fields": "植物生理学／食虫植物／バイオエネルギー",
+      "dept": "応用生物化学科",
+      "passwordHash": "03b2379e593ca7834e7a22370569055619ac5273da9bac4b473f0254603943ed"
+    },
+    {
+      "id": "mohnishi",
+      "name": "大西素子研究室",
+      "building": "30号館",
+      "floor": "8F",
+      "days": [
+        9
+      ],
+      "fields": "生化学／分子生物学",
+      "dept": "応用生物化学科",
+      "passwordHash": "fdae06bfa5e5236ca22c12af17bd285af096dc6ce2dddb4526d64d502810cb1f"
+    },
+    {
+      "id": "kojima",
+      "name": "小島晶子研究室",
+      "building": "30号館",
+      "floor": "7F",
+      "days": [
+        8,
+        9
+      ],
+      "fields": "植物分子生物学／分子生物学",
+      "dept": "環境生物科学科",
+      "passwordHash": "f10123b68cc4de8f323dc8e7d884965d4879592c08b4bb9b023fba199397ab86"
+    },
+    {
+      "id": "kanemasa",
+      "name": "金政 真研究室",
+      "building": "30号館",
+      "floor": "7F",
+      "days": [
+        7,
+        8,
+        9
+      ],
+      "fields": "応用微生物学／分子生物学",
+      "dept": "応用生物化学科",
+      "passwordHash": "9d0bd2ca0e2d322e234ef3b1cc5edc16972cbeca29d8a4c0fc728cf30766c0c6"
+    },
+    {
+      "id": "sakano",
+      "name": "坂野弘美研究室",
+      "building": "30号館",
+      "floor": "7F",
+      "days": [
+        7,
+        9
+      ],
+      "fields": "植物分子生理学",
+      "dept": "環境生物科学科",
+      "passwordHash": "db2ad7c60ecc550a674ddd8546013bd4140f2e63f8b095fa90693137d653e14f"
+    },
+    {
+      "id": "yamashita",
+      "name": "山下紗也加研究室",
+      "building": "30号館",
+      "floor": "2F",
+      "days": [
+        8
+      ],
+      "fields": "公衆栄養学",
+      "dept": "管理栄養科学専攻",
+      "passwordHash": "2c6f02d217e79a14050e01054e38b24951ef9247e20c7706ec4de48e3061e721"
+    },
+    {
+      "id": "matsumoto",
+      "name": "松本吉博研究室",
+      "building": "33号館",
+      "floor": "7F",
+      "days": [
+        8
+      ],
+      "fields": "分子生物学／DNA複製・修復／抗癌剤の作用",
+      "dept": "応用生物化学科",
+      "passwordHash": "9e4b131f46dbd7bfc163a7d3c3552c9d93043a27f0d2b01d5a9a21a7284ebeed"
+    },
+    {
+      "id": "hodoki",
+      "name": "程木義邦研究室",
+      "building": "33号館",
+      "floor": "7F",
+      "days": [
+        7,
+        8,
+        9
+      ],
+      "fields": "陸水学／微生物生態学／保全生物学",
+      "dept": "環境生物科学科",
+      "passwordHash": "7558010cfa88112f71611c2bcd1087a02343172e9c64954af70dbc62f98ee40a"
+    },
+    {
+      "id": "minami",
+      "name": "南 基泰研究室",
+      "building": "33号館",
+      "floor": "6F",
+      "days": [
+        7,
+        8
+      ],
+      "fields": "薬用植物学／分子生物学／生物多様性評価",
+      "dept": "環境生物科学科",
+      "passwordHash": "43dda4329ded480e7f66fbaffa241a674e30c5e0ee3f2ee5ded21f1bec1d2fc1"
+    },
+    {
+      "id": "yoshimura",
+      "name": "吉村和也研究室",
+      "building": "33号館",
+      "floor": "6F",
+      "days": [
+        7,
+        8,
+        9
+      ],
+      "fields": "植物生理学",
+      "dept": "環境生物科学科",
+      "passwordHash": "260e9fff94472c599fe005caf57d6697bc2135cd340feb4789686accc8716ccb"
+    },
+    {
+      "id": "nakagawa",
+      "name": "中川大研究室",
+      "building": "33号館",
+      "floor": "5F",
+      "days": [
+        7
+      ],
+      "fields": "細胞工学／生理遺伝学／薬理活性物質化学",
+      "dept": "応用生物化学科",
+      "passwordHash": "ca1c6c6a08f0b9d86ec5019a84ff9d709849be46b86b4d27b65accca408a7a04"
+    },
+    {
+      "id": "tsuda",
+      "name": "津田孝範研究室",
+      "building": "33号館",
+      "floor": "4F",
+      "days": [
+        7,
+        8,
+        9
+      ],
+      "fields": "食品機能学",
+      "dept": "食品栄養科学専攻",
+      "passwordHash": "3c72b372c329dedb8d0a983b09f478ad44b143bfcea495c4bbc93762669788f0"
+    },
+    {
+      "id": "kanamaru",
+      "name": "金丸京子研究室",
+      "building": "33号館",
+      "floor": "4F",
+      "days": [
+        8
+      ],
+      "fields": "コウジカビ／生存戦略／発酵の仕組み",
+      "dept": "応用生物化学科",
+      "passwordHash": "fcfed2cd31f5377727086634b863b7c285379cb9acf5909cfa0ffb5c0ee01469"
+    },
+    {
+      "id": "miyata",
+      "name": "宮田 茂研究室",
+      "building": "33号館",
+      "floor": "4F",
+      "days": [
+        9
+      ],
+      "fields": "分子微生物学／病原微生物学／応用微生物学",
+      "dept": "管理栄養科学専攻",
+      "passwordHash": "529ba837572c62f8058f4ca7562dc9f8f9bffc9ad14130f940d4cfd7d21b6acd"
+    },
+    {
+      "id": "tsuchida",
+      "name": "土田さやか研究室",
+      "building": "32号館",
+      "floor": "1F",
+      "days": [
+        7,
+        8
+      ],
+      "fields": "細菌学／衛生学／実験動物学",
+      "dept": "応用生物化学科",
+      "passwordHash": "8e9acb536f85ab6cefe7cd7bdda2b6d0c414dbc8783dcb611afd599de22acf3d"
+    },
+    {
+      "id": "maeno",
+      "name": "前野善孝研究室",
+      "building": "11号館",
+      "floor": "3F",
+      "days": [
+        7
+      ],
+      "fields": "細胞生理学／食品成分機能学",
+      "dept": "管理栄養科学専攻",
+      "passwordHash": "040544e10c619910db564c87857a978dac71a3f4679dd0966f2c21031b6749b3"
+    },
+    {
+      "id": "kusano",
+      "name": "草野由理研究室",
+      "building": "11号館",
+      "floor": "3F",
+      "days": [
+        7
+      ],
+      "fields": "食品機能学",
+      "dept": "管理栄養科学専攻",
+      "passwordHash": "33073656a83f748426483fb1ae2decb452fbc9c55dc9177babae3437cbb6705f"
+    },
+    {
+      "id": "kozai",
+      "name": "香西はな研究室",
+      "building": "11号館",
+      "floor": "2F",
+      "days": [
+        7
+      ],
+      "fields": "給食経営管理／調理科学",
+      "dept": "管理栄養科学専攻",
+      "passwordHash": "977abb6fee58673ad55cab198fa920aa485b7c0ea1b554d815b313265881e91f"
+    },
+    {
+      "id": "dohmae",
+      "name": "堂前純子研究室",
+      "building": "11号館",
+      "floor": "2F",
+      "days": [
+        8
+      ],
+      "fields": "生化学／病理学／分子生物学",
+      "dept": "管理栄養科学専攻",
+      "passwordHash": "5e6929ff1053bb9c54b8c8ddccdbf890001e7872124d401c81343e238c77cd11"
+    },
+    {
+      "id": "sakuma",
+      "name": "佐久間直緒美研究室",
+      "building": "11号館",
+      "floor": "3F",
+      "days": [
+        8
+      ],
+      "fields": "栄養教育／食育／栄養教諭",
+      "dept": "管理栄養科学専攻",
+      "passwordHash": "3195f043771fcfe1519bb7068e69bb481b0c3902ceaa0b87759b21354c9b6187"
+    },
+    {
+      "id": "tanaka",
+      "name": "田中 守研究室",
+      "building": "11号館",
+      "floor": "2F",
+      "days": [
+        9
+      ],
+      "fields": "食品免疫学／食品機能学／健康科学",
+      "dept": "管理栄養科学専攻",
+      "passwordHash": "beb553d9fdb9f4193496d44416646b044a2789d6590e7f63effc3e7af73f9439"
+    },
+    {
+      "id": "koike",
+      "name": "小池温子研究室",
+      "building": "11号館",
+      "floor": "1F",
+      "days": [
+        9
+      ],
+      "fields": "スポーツ栄養学／応用栄養学／運動生理学（12:00-14:00）",
+      "dept": "管理栄養科学専攻",
+      "passwordHash": "0057ee66cbd6137ae951204aa6d9460fb494f3357022182bf60eda33e2ed5303"
+    },
+    {
+      "id": "yamanaka",
+      "name": "山中由実研究室",
+      "building": "33号館北プレハブ棟",
+      "floor": "2F",
+      "days": [
+        9
+      ],
+      "fields": "調理科学",
+      "dept": "管理栄養科学専攻",
+      "passwordHash": "8e3438fb230a16b6fcf5f701f8a895097051ab94425aba22616ad3f11b2b6427"
+    }
+  ],
+  "buildingOrder": [
+    "30号館",
+    "32号館",
+    "33号館",
+    "11号館",
+    "33号館北プレハブ棟"
+  ],
+  "floorOrder": {
+    "30号館": [
+      "11F",
+      "10F",
+      "9F",
+      "8F",
+      "7F",
+      "2F"
+    ],
+    "32号館": [
+      "1F"
+    ],
+    "33号館": [
+      "7F",
+      "6F",
+      "5F",
+      "4F"
+    ],
+    "11号館": [
+      "3F",
+      "2F",
+      "1F"
+    ],
+    "33号館北プレハブ棟": [
+      "2F"
+    ]
+  }
+};
